6bd046f3e209a21fa8a2a9a5c9dc4b66b99bfcdf
Maurice1980 <klikobak0@gmail.com>
2025-11-30 10:27:51 +0100

    Fix catch-all route for Render deployment - change '*' to '/*' for path-to-regexp compatibility

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ app.get('/health', (req, res) => {
 });
 
 // Catch-all handler: send back React's index.html file for client-side routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
